431943e0aa0cade63d6116087adc28c7d3d08fc5
thiago-win10 <thiago.soares.dev@gmail.com>
2021-11-30 18:39:19 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Migrations/20211130212419_OtherEntities.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class OtherEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SellerId",
+                table: "Department",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Seller",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    BaseSalary = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seller", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalesRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Amount = table.Column<double>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    SellerId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SalesRecords_Seller_SellerId",
+                        column: x => x.SellerId,
+                        principalTable: "Seller",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Department_SellerId",
+                table: "Department",
+                column: "SellerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesRecords_SellerId",
+                table: "SalesRecords",
+                column: "SellerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Seller_SellerId",
+                table: "Department",
+                column: "SellerId",
+                principalTable: "Seller",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Seller_SellerId",
+                table: "Department");
+
+            migrationBuilder.DropTable(
+                name: "SalesRecords");
+
+            migrationBuilder.DropTable(
+                name: "Seller");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Department_SellerId",
+                table: "Department");
+
+            migrationBuilder.DropColumn(
+                name: "SellerId",
+                table: "Department");
+        }
+    }
+}
--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,4 +1,8 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+namespace SalesWebMvc.Models
 {
     public class Department
     {
@@ -6,5 +10,28 @@
         public string Name { get; set; }
 
         public double Valor { get; set; }
+        [NotMapped]
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name, double valor)
+        {
+            Id = id;
+            Name = name;
+            Valor = valor;
+        }
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
+   
 }
